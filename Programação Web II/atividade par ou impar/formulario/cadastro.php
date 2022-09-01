7dd3dd0d456e17918410c88d6a29d3a196e6d4b2
Fabio-Vini <88862357+Fabio-Vini@users.noreply.github.com>
2022-09-01 19:34:55 -0300

    Delete Programação Web II/atividade par ou impar directory

--- Programação Web II/atividade par ou impar/formulario/cadastro.php
+++
@@ -1,7 +0,0 @@
-<?php
-if($_POST){
-    echo"Nome: ".$_POST['nome'].'<br>';
-    echo"Email: ".$_POST['email'].'<br>';
-    echo"Assunto: ".$_POST['assunto'].'<br>';
-    echo"Mensagem: <br>".$_POST['mensagem'].'<br>';
-}
